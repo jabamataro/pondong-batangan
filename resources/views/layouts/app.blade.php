8fc26247b9b91aae7e57ca1a5d3c0e7a6a3b5d71
Jaba Mataro <jmataro9@gmail.com>
2018-10-21 02:04:33 +0800

    Updated Programs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    {{-- <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+    {{--
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> --}}
     <title>{{ $siteTitle }}</title>
 
 </head>
